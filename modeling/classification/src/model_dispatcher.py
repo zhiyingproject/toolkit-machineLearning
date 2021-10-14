dc4d07b89a9e070cdbde4699b6a2367cc4c875b7
zhiyingproject <zhiying.li.krems@gmail.com>
2021-10-13 18:03:07 -0700

    added model dispatcher

--- modeling/classification/src/model_dispatcher.py
+++ modeling/classification/src/model_dispatcher.py
@@ -1,5 +1,10 @@
 from sklearn import tree
 from sklearn import ensemble
+from sklearn.gaussian_process import GaussianProcessClassifier
+from sklearn.gaussian_process.kernels import RBF
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+
 
 models = {
     'decision_tree_gini': tree.DecisionTreeClassifier(
@@ -9,4 +14,11 @@ models = {
         criterion='entropy'
     ),
     'rf': ensemble.RandomForestClassifier(),
+    'ada_boost': ensemble.AdaBoostClassifier(),
+    'gp': GaussianProcessClassifier(
+        1.0 * RBF(1.0)
+    ),
+    'knn': KNeighborsClassifier(10),
+    'svc_linear': SVC(kernel='linear', C=0.025),
+    'svc_none_linear': SVC(gamma=2, C=1)
 }
